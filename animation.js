b8fbb3340b14ec22fc9e1cf4e0504a3beb168f64
Dhivyesh <dhivyeshrathi@gmail.com>
2023-01-14 13:45:31 -0600

    json

---
+++ animation.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $(window).on("scroll", function(){
+        $(".fade-in").each(function(){
+            /* Check if the element is in view */
+            if(isInView($(this))){
+                /* Add the class "in-view" to the element */
+                $(this).addClass("in-view");
+            }
+        });
+    });
+});
+
+/* Function to check if an element is in view */
+function isInView(elem){
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
